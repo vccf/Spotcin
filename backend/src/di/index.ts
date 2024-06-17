fd335f7ea6e77e7bf1dc8af2c85129adfb465d65
Thiago de Souza Farias <tsf@cin.ufpe.br>
2024-06-17 14:33:36 -0300

    feat: implement user routes

--- backend/src/di/index.ts
+++ backend/src/di/index.ts
@@ -2,16 +2,23 @@ import OtherRepository from '../repositories/other.repository';
 import TestRepository from '../repositories/test.repository';
 import TestService from '../services/test.service';
 import Injector from './injector';
+import UserRepository from '../repositories/user.repository';
+import UserService from '../services/user.service';
 
 export const di = new Injector();
 
 // Test
 di.registerRepository(TestRepository, new TestRepository());
 di.registerRepository(OtherRepository, new OtherRepository());
+di.registerRepository(UserRepository, new UserRepository());
 di.registerService(
   TestService,
   new TestService(
     di.getRepository(TestRepository),
     di.getRepository(OtherRepository)
-  )
-);
+  ));
+di.registerService(
+  UserService,
+  new UserService(
+    di.getRepository(UserRepository)
+  ));
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,8 @@ import { Express, Router } from 'express';
 import { di } from '../di';
 import TestController from '../controllers/test.controller';
 import TestService from '../services/test.service';
+import UserController from '../controllers/user.controller';
+import UserService from '../services/user.service';
 
 const router = Router();
 const prefix = '/api';
@@ -9,6 +11,11 @@ const prefix = '/api';
 export default (app: Express) => {
   app.use(
     prefix,
+    new UserController(router, di.getService(UserService)).router
+  );
+  app.use(
+    prefix,
     new TestController(router, di.getService(TestService)).router
   );
 };
+
