57631ef1948471c6200cb4958da24adf474871cb
Flávio Roberto <124847034+FlavioRobertoTavares@users.noreply.github.com>
2024-06-17 15:41:19 -0300

    feat: criação da entidade, modelo e repositório das playlists. Adição de novos erros no util.

---
+++ backend/src/repositories/playlist.repository.ts
@@ -0,0 +1,104 @@
+import PlaylistEntity from '../entities/playlist.entity';
+import { InternalServerError } from '../utils/errors/http.error';
+import BaseRepository from './base.repository';
+
+class PlaylistRepository extends BaseRepository<PlaylistEntity> {
+  constructor() {
+    super('playlists');
+  }
+
+  public async getPlaylists(): Promise<PlaylistEntity[]> {
+    try {
+      return await this.findAll();
+    } catch (e) {
+      throw new InternalServerError();
+    }
+  }
+
+  public async getPlaylistById(id: string): Promise<PlaylistEntity | undefined> {
+    try {
+      let playlists = await this.findAll();
+      let playlist = playlists.find((playlist) => playlist.id === id);
+
+      return playlist;
+    } catch (e) {
+      throw new InternalServerError();
+    }
+  }
+
+  public async getPlaylistByName(name: string): Promise<PlaylistEntity | undefined> {
+    try {
+      let playlists = await this.findAll();
+      let playlist = playlists.find((playlist) => playlist.name === name);
+
+      return playlist;
+    } catch (e) {
+      throw new InternalServerError();
+    }
+  }
+
+  public async deletePlaylistById(id: string): Promise<void> {
+    try {
+      const playlist = await this.getPlaylistById(id);
+      
+      if (playlist) {
+        await this.delete((playlist) => playlist.id === id);
+      }
+    } catch (e) {
+      throw new InternalServerError();
+    }
+  }
+  
+  public async deletePlaylistByName(name: string): Promise<void> {
+    try {
+      const playlist = await this.getPlaylistByName(name);
+      
+      if (playlist) {
+        await this.delete((playlist) => playlist.name === name);
+      }
+    } catch (e) {
+      throw new InternalServerError();
+    }
+  }
+  
+  //Talvez esteja errada
+  public async updatePlaylistById(newPlaylist: PlaylistEntity, id: string): Promise<PlaylistEntity | undefined> {
+    try {
+      let nPlaylist = await this.update((playlist) => playlist.id === id, newPlaylist);
+      if (nPlaylist === null) {
+        return undefined;
+      }
+      return nPlaylist;
+    } catch (e) {
+      throw new InternalServerError();
+    }
+  }
+  
+  public async updatePlaylistByName(newPlaylist: PlaylistEntity, name: string): Promise<PlaylistEntity | undefined> {
+    try {
+      let nPlaylist = await this.update((playlist) => playlist.name === name, newPlaylist);
+      if (nPlaylist === null) {
+        return undefined;
+      }
+      return nPlaylist;
+    } catch (e) {
+      throw new InternalServerError();
+    }
+  }
+
+  public async createPlaylist(newPlaylist: PlaylistEntity): Promise<PlaylistEntity | undefined> {
+    try {
+      let nPlaylist = await this.add(newPlaylist);
+
+      return nPlaylist;
+    } catch (e) {
+      throw new InternalServerError();
+    }
+  }
+  
+  //Delete e Add song por fazer
+
+
+
+}
+export default new PlaylistRepository();
--- backend/src/utils/errors/http.error.ts
+++ backend/src/utils/errors/http.error.ts
@@ -72,3 +72,49 @@ export class HttpNotImplementedError extends HttpError {
     });
   }
 }
+
+export class BadRequestError extends HttpError {
+  constructor({ msg, msgCode }: { msg: string; msgCode?: any }) {
+    super({ status: 400, msg, msgCode, slug: 'bad-request' });
+  }
+}
+
+export class UnauthorizedError extends HttpError {
+  constructor({ msg, msgCode }: { msg: string; msgCode?: any }) {
+    super({ status: 401, msg, msgCode, slug: 'unauthorized' });
+  }
+}
+
+export class ForbiddenError extends HttpError {
+  constructor({ msg, msgCode }: { msg: string; msgCode?: any }) {
+    super({ status: 403, msg, msgCode, slug: 'forbidden' });
+  }
+}
+
+export class NotFoundError extends HttpError {
+  constructor({ msg, msgCode }: { msg: string; msgCode?: any }) {
+    super({ status: 404, msg, msgCode, slug: 'not-found' });
+  }
+}
+
+export class InternalServerError extends HttpError {
+  constructor({ msg, msgCode }: { msg?: string; msgCode?: any } = {}) {
+    super({
+      status: 500,
+      msg: msg || 'Internal Server Error',
+      msgCode,
+      slug: 'internal-server',
+    });
+  }
+}
+
+export class NotImplementedError extends HttpError {
+  constructor({ msg, msgCode }: { msg?: string; msgCode?: any } = {}) {
+    super({
+      status: 501,
+      msg: msg || 'Not Implemented Error',
+      msgCode,
+      slug: 'not-implemented',
+    });
+  }
+}
